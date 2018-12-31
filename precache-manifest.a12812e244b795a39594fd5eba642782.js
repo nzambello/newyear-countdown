b4ce265131efc6266e745af40be7fd33e9da14d3
nzambello <nicola.zambello@redturtle.it>
2018-12-31 20:58:22 +0100

    Updates

---
+++ precache-manifest.a12812e244b795a39594fd5eba642782.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cc7ca69faaeb07c9001",
+    "url": "nzambello.github.io/newyear-countdown/static/css/main.fd409b60.chunk.css"
+  },
+  {
+    "revision": "4cc7ca69faaeb07c9001",
+    "url": "nzambello.github.io/newyear-countdown/static/js/main.4cc7ca69.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "nzambello.github.io/newyear-countdown/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "d002d04edbb633746cd5",
+    "url": "nzambello.github.io/newyear-countdown/static/js/runtime~main.d002d04e.js"
+  },
+  {
+    "revision": "8f9991ea2b44984c96a90d667e67949b",
+    "url": "nzambello.github.io/newyear-countdown/index.html"
+  }
+];
