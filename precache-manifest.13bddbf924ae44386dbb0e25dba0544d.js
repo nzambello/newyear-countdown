866d3d0afe534e20fced328002b2cb5747c290fe
nzambello <nzambello@protonmail.com>
2020-12-25 19:55:33 +0100

    Updates

---
+++ precache-manifest.13bddbf924ae44386dbb0e25dba0544d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d002d04edbb633746cd5",
+    "url": "nzambello.github.io/newyear-countdown/static/js/runtime~main.d002d04e.js"
+  },
+  {
+    "revision": "86c8bfb64bdd94069c44",
+    "url": "nzambello.github.io/newyear-countdown/static/js/main.86c8bfb6.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "nzambello.github.io/newyear-countdown/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "86c8bfb64bdd94069c44",
+    "url": "nzambello.github.io/newyear-countdown/static/css/main.d6a6e0c9.chunk.css"
+  },
+  {
+    "revision": "ba49c40262a35057daab5de22415b41d",
+    "url": "nzambello.github.io/newyear-countdown/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92452ff3274a0ca08f8762a15f78c012.js"
+  "nzambello.github.io/newyear-countdown/precache-manifest.13bddbf924ae44386dbb0e25dba0544d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("nzambello.github.io/newyear-countdown/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
